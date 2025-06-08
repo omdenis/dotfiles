8697a045e66b2bb661a3898b8a08ca9a960ed6c3
omdenis <millerdenis@gmail.com>
2025-06-08 19:03:06 +0200

    ffmeg: 3 formats

--- FedoraXFCE/bin/video_downloader.sh
+++ FedoraXFCE/bin/video_downloader.sh
@@ -4,11 +4,10 @@
 BASE_DIR="$HOME/video"
 INPUT_FILE="$BASE_DIR/files.txt"
 TEMP_DIR="$BASE_DIR/01_downloaded"
-SLIDES_DIR="$BASE_DIR/02_slides"
 FFMPEG="$HOME/apps/ffmpeg/ffmpeg"
 
 # Create directories and input file if they don't exist
-mkdir -p "$TEMP_DIR" "$SLIDES_DIR"
+mkdir -p "$TEMP_DIR" 
 touch "$INPUT_FILE"
 
 echo "📁 Working directories:"
@@ -17,13 +16,16 @@ echo " - Encoded slides: $SLIDES_DIR"
 echo " - URL list file: $INPUT_FILE"
 echo
 
+exec 3< "$INPUT_FILE"  
+
 COUNTER=1
 
 pad_number() {
     printf "%03d" "$1"
 }
 
-while IFS= read -r url || [[ -n "$url" ]]; do
+while IFS= read -r url <&3; do
+# while IFS= read -r url || [[ -n "$url" ]]; do
     url="$(echo "$url" | xargs)"  # trim leading/trailing whitespace
     [[ -z "$url" || "$url" =~ ^# ]] && continue  # skip empty lines or comments
 
@@ -53,11 +55,14 @@ while IFS= read -r url || [[ -n "$url" ]]; do
     fi
 
     OUTPUT_NAME="${NUM}_${SAFE_NAME}.mp4"
-    OUTPUT_PATH="$SLIDES_DIR/$OUTPUT_NAME"
 
-    echo "📦 Re-encoding for Telegram..."
+    echo "📦 Re-encoding for Telegram: slides and presentations"
+    TYPE="02_slides"
+    OUTPUT_PATH="$BASE_DIR/$TYPE/$OUTPUT_NAME"
+    mkdir -p "$BASE_DIR/$TYPE/"
     "$FFMPEG" -y -i "$FILENAME" \
         -hide_banner \
+        -nostats \
         -loglevel error \
         -threads 4 \
         -map_metadata -1 \
@@ -68,13 +73,66 @@ while IFS= read -r url || [[ -n "$url" ]]; do
         -vcodec libx264 \
         -profile:v main \
         -pix_fmt yuv420p \
-        -c:a aac -b:a 64k -ac 1 \
+        -c:a aac -ac 1 -b:a 64k \
         -tune stillimage \
         -preset faster \
         -movflags +faststart \
         "$OUTPUT_PATH" < /dev/null
-
     echo "✅ Saved: $OUTPUT_PATH"
+    ffprobe -i "$OUTPUT_PATH" -hide_banner
+    echo
+    echo
+
+    echo "📦 Re-encoding for Telegram: high quality video for mobile devices"
+    TYPE="03_mobile"
+    OUTPUT_PATH="$BASE_DIR/$TYPE/$OUTPUT_NAME"
+    mkdir -p "$BASE_DIR/$TYPE/"
+    "$FFMPEG" -y -i "$FILENAME" \
+        -hide_banner \
+        -nostats \
+        -loglevel error \
+        -threads 4 \
+        -map_metadata -1 \
+        -max_muxing_queue_size 512 \
+        -filter:v "fps=20,scale=iw/2:ih/2:flags=lanczos" \
+        -crf 23 \
+        -c:v libx264 \
+        -preset slow \
+        -profile:v main \
+        -pix_fmt yuv420p \
+        -c:a aac -ac 1 -b:a 64k \
+        -movflags +faststart \
+        "$OUTPUT_PATH" < /dev/null
+    echo "✅ Saved: $OUTPUT_PATH"
+    ffprobe -i "$OUTPUT_PATH" -hide_banner
+    echo
+    echo
+
+    echo "📦 Re-encoding for Telegram: slides and presentations (/2 size)"
+    TYPE="02_slides_x2"
+    OUTPUT_PATH="$BASE_DIR/$TYPE/$OUTPUT_NAME"
+    mkdir -p "$BASE_DIR/$TYPE/"
+    "$FFMPEG" -y -i "$FILENAME" \
+        -hide_banner \
+        -nostats \
+        -loglevel error \
+        -threads 4 \
+        -map_metadata -1 \
+        -max_muxing_queue_size 512 \
+        -filter:v "fps=2,scale=iw/2:ih/2:flags=lanczos" \
+        -crf 30 \
+        -r 2 \
+        -vcodec libx264 \
+        -profile:v main \
+        -pix_fmt yuv420p \
+        -c:a aac -ac 1 -b:a 64k \
+        -tune stillimage \
+        -preset faster \
+        -movflags +faststart \
+        "$OUTPUT_PATH" < /dev/null
+    echo "✅ Saved: $OUTPUT_PATH"
+    ffprobe -i "$OUTPUT_PATH" -hide_banner
+    echo
     echo
 
     ((COUNTER++))
@@ -82,3 +140,4 @@ while IFS= read -r url || [[ -n "$url" ]]; do
 done < "$INPUT_FILE"
 
 echo "🎉 All done! Encoded videos are in '$SLIDES_DIR'."
+exec 3<&-
